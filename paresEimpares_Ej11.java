f576fd2bf7030d7df79fbc195a9e5ab47eb5435f
Orsted <felipe.t.m.77@gmail.com>
2018-11-21 10:09:25 -0600

    Aqui se hiso que se dejaran de mostrar 0 del array

--- paresEimpares_Ej11.java
+++ paresEimpares_Ej11.java
@@ -14,30 +14,55 @@ import static e1.apoyaIncercion.Insercion;
  * @author Phill
  */
 public class paresEimpares_Ej11 {
-  
 
-    
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        apoyoBurbuja bj =new apoyoBurbuja();
+        apoyaIncercion in = new apoyaIncercion();
+        Scanner sc = new Scanner(System.in);
         System.out.println("Cuantos numeros deseas ingresar? ");
         int a = sc.nextInt();
-        int arreglo[] =  new int[a];
+        int arreglo[] = new int[a];
         int x[] = new int[a];
-        int c[] = new int [a];
-        int q=0, w =0;
+        int c[] = new int[a];
+        int q = 0, w = 0;
         for (int i = 0; i < arreglo.length; i++) {
-           arreglo[i]= sc.nextInt();
-           
-           if(arreglo[i]%2==0){
-           c[q]=arreglo[i];
-           q++;
-           }else if(arreglo[i]%2!=0){
-            x[w] = arreglo[i];
-            w++;
+            arreglo[i] = sc.nextInt();
+
+            if (arreglo[i] % 2 == 0) {
+                c[q] = arreglo[i];
+                q++;
+            } else if (arreglo[i] % 2 != 0) {
+                x[w] = arreglo[i];
+                w++;
+            }
+
+          x=in.Insercion2(x);
+          c=bj.burbuja(c);  
         }
-            
-    }
-       System.out.print(Arrays.toString(x));
-        System.out.print(Arrays.toString(c));
+        System.out.println("Arreglo original: ");
+        for (int i = 0; i < arreglo.length; i++) {
+
+            System.out.print(arreglo[i] + ",");
+
+        }
+        System.out.println("");
+        System.out.println("Pares ordenados: ");
+        for (int i = 0; i < arreglo.length; i++) {
+            if (c[i]>0) {
+                System.out.print(c[i] + ",");
+            }
+        }
+        System.out.println("");
+        System.out.println("Impares ordenados: ");
+        for (int i = 0; i < arreglo.length; i++) {
+            if (x[i] > 0) {
+                System.out.print(x[i] + ",");
+            }
+        }
+        System.out.println("");
+        
+
+        //System.out.print(Arrays.toString(x));
+        //System.out.print(Arrays.toString(c));
     }
 }
