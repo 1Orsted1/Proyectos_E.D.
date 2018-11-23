1a3862e77b4376270403e83aae77311f485a2da6
Orsted <felipe.t.m.77@gmail.com>
2018-11-22 21:02:17 -0600

    Vercion definitiva(1.6)

--- paresEimpares_Ej11.java
+++ paresEimpares_Ej11.java
@@ -1,17 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Version final del programa(podria ser peor)
  */
 package e1;
 
 import java.util.Arrays;
 import java.util.Scanner;
-import static e1.apoyaIncercion.Insercion;
 
 /**
  *
- * @author Phill
+ * @author Orsted
  */
 public class paresEimpares_Ej11 {
 
@@ -27,56 +24,52 @@ public class paresEimpares_Ej11 {
             a = sc.nextInt();
 
         } while (a < 10);
-
-
-//       System.out.println("Cuantos numeros deseas ingresar? ");
-//        int a = sc.nextInt();
        
-        
         int arreglo[] = new int[a];
-        int x[] = new int[a];
-        int c[] = new int[a];
+      
         int q = 0, w = 0;
-        //unico for de asignacion
+        //for de asignacion del arreglo original
         for (int i = 0; i < arreglo.length; i++) {
             arreglo[i] = sc.nextInt();
 
             if (arreglo[i] % 2 == 0) {
-                c[q] = arreglo[i];
+                
                 q++;
             } else if (arreglo[i] % 2 != 0) {
-                x[w] = arreglo[i];
+                
                 w++;
             }
-        //ordenamientos
-          x=in.Insercion2(x);
-          c=bj.burbuja(c);  
+          
         }
-        //impreciones de arreglos
-        System.out.println("Arreglo original: ");
+        int x[] = new int[w];
+        int c[] = new int[q];
+        int h=0,s=0;
+        //for de asignacion de sub arreglos pares e impares
         for (int i = 0; i < arreglo.length; i++) {
-
-            System.out.print(arreglo[i] + ",");
-
-        }
-        System.out.println("");
-        System.out.println("Pares ordenados: ");
-        for (int i = 0; i < arreglo.length; i++) {
-            if (c[i]>0) {
-                System.out.print(c[i] + ",");
+               
+            if (arreglo[i] % 2 == 0) {
+                c[h] = arreglo[i];
+                h++;
+            } 
+             if (arreglo[i] % 2 != 0) {
+                x[s] = arreglo[i];
+                s++;
             }
-        }
-        System.out.println("");
-        System.out.println("Impares ordenados: ");
-        for (int i = 0; i < arreglo.length; i++) {
-            if (x[i] > 0) {
-                System.out.print(x[i] + ",");
-            }
-        }
-        System.out.println("");
         
-        //otra forma de hacerlo
-        //System.out.print(Arrays.toString(x));
-        //System.out.print(Arrays.toString(c));
+        }
+          //ordenamientos
+          arreglo=in.Insercion2(arreglo);
+          x=in.Insercion2(x);
+          c=bj.burbuja(c);
+
+        
+        //impresion de arreglos
+        System.out.println("\nArreglo original ordenado: ");
+        System.out.print(Arrays.toString(arreglo)+" \n");
+        System.out.println("Arreglo de pares: ");
+        System.out.print(Arrays.toString(c)+" \n");
+        System.out.println("Arreglo de impres:");
+        System.out.print(Arrays.toString(x)+" \n");
+        
     }
 }
